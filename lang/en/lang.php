3a83bd9f731765c6748178fb50baf9ee3ee055c5
gergo85 <gergo85@gmail.com>
2015-04-08 14:32:14 +0200

    Improve localisation

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,17 +1,21 @@
 <?php
 
 return [
+    'plugin' => [
+        'name'               => "Changelog",
+        'description'        => "Adds a report widget to show the October CMS changelog on the dashboard."
+    ],
     'app' => [
-        'permission_error'   => "You don't have permission to manage updates",
+        'permission_error'   => "You don't have permission to manage updates"
     ],
     'log' => [
         'widget_title'       => "System changes",
         'load_error'         => "Could not load changelog from %s",
-        'slice_error'        => "Unable to slice changelog, build %s not found.",
+        'slice_error'        => "Unable to slice changelog, build %s not found."
     ],
     'recentLogs' => [
         'label'              => "Recent logs",
         'description'        => "When the system is up to date, this sets how many recent logs are displayed.",
-        'validation_message' => "Recent logs must be a positive number",
-    ],
+        'validation_message' => "Recent logs must be a positive number"
+    ]
 ];
